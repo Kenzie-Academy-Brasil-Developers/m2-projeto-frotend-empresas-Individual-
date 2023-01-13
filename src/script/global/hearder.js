00c52df93bceb695479c72dc1d21ac864c39b19f
Vinicius <viniciusribeiro.dev@hotmail.com>
2023-01-13 13:59:42 -0300

    voltando tudo ao normal

--- src/script/global/hearder.js
+++ src/script/global/hearder.js
@@ -12,35 +12,18 @@ export function clickMenu(menu, nav) {
   }
 }
 
-
-
 export function clickLogin() {
-  let verf = window.location.pathname;
-  if (verf.includes("/m2-projeto-frotend-empresas-Individual-")){
-     window.location.replace("/m2-projeto-frotend-empresas-Individual-/src/pages/login.html");
-  } else {
-    window.location.replace("/src/pages/login.html")
-  }
+  window.location.replace("/src/pages/login.html");
 }
 
 export function clickRegistration() {
-  let verf = window.location.pathname;
-  if (verf.includes("/m2-projeto-frotend-empresas-Individual-")){
-     window.location.replace("/m2-projeto-frotend-empresas-Individual-/src/pages/registration.html");
-  } else {
-    window.location.replace("/src/pages/registration.html")
-  }
+  window.location.replace("/src/pages/registration.html");
 }
 
 export function clickHome() {
-  window.location.replace("/m2-projeto-frotend-empresas-Individual-/");
+  window.location.replace("/");
 }
 
 export function clickLogout() {
-  let verf = window.location.pathname;
-  if (verf.includes("/m2-projeto-frotend-empresas-Individual-")){
-     window.location.replace("/m2-projeto-frotend-empresas-Individual-/src/pages/login.html");
-  } else {
-    window.location.replace("/src/pages/login.html")
-  }
+  window.location.replace("/src/pages/login.html");
 }
